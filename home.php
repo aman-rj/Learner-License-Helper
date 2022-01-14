d5d9113f857c2f2660d6a8cb17a58da17784df6c
Academics Helper <78408604+aman-rj@users.noreply.github.com>
2022-01-15 02:32:16 +0530

    major changes in the entire project

--- home.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head> <title> HOME | LEARNER'S LICENSE HELPER </title> </head>
-<frameset rows = "32%,*,23%" border="0">
-<frame src= "a.php" scrolling="no">
-<frame src= "b.php" scrolling="no">
-<frame src= "c.php" scrolling="no">
-</frameset>
-</html>
